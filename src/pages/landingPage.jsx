122f6fd6b76226b0bc0c621c9adcd8e554493f14
Muhammad Issa <muhammadissa848@gmail.com>
2025-01-26 07:41:41 +0500

    LAND

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -35,7 +35,7 @@ const LoanCategories = [
   }
 ];
 
-const landingPage = () => {
+const LandingPage = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedSubcategory, setSelectedSubcategory] = useState(null);
   const [loanAmount, setLoanAmount] = useState(0);
@@ -188,4 +188,4 @@ const landingPage = () => {
   );
 };
 
-export default landingPage;
+export default LandingPage;
